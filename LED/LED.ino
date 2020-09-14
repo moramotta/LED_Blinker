f0bf918fbf539560b89fec1277954aff1a952bee
moramotta <gert@moramotta.de>
2020-09-14 13:29:02 +0200

    Create LED.ino

---
+++ LED/LED.ino
@@ -0,0 +1,26 @@
+/*
+
+  Programm LED
+  Eine LED blinkt im Sekundentakt
+  MRA 2020
+  Version 0.1
+  LED angeschlossen an Pin 3
+  letzte Änderung 14.09.2020
+
+*/
+
+#declare LED 3   //grüne LED an Pin 3 
+
+void setup()
+{
+  pinMode(LED, OUTPUT); //LED
+}
+
+void loop()
+{
+  /*************** LED blinkt im Sekundentakt  ********************/
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
